260df8c2c542e4bedb3de066ce30e67e687f3463
NaoiseOL <G00385498@atu.ie>
2024-12-02 15:11:49 +0000

    Added variables for Player Class

--- src/main/java/com/example/cicdproject/Player.java
+++ src/main/java/com/example/cicdproject/Player.java
@@ -1,9 +1,6 @@
 package com.example.cicdproject;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +10,34 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table
-public class Player {
+public class Player{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String firstname;
+    private String surname;
+    private int age;
+    private String DoB;
+    private String nationality;
+    private int heightinMetres;
+    private String position;
+    private String foot;
+    private String club;
+    private int marketValue;
 
 
+    public Player(String id,String firstname , String surname, int age, String DoB, String nationality, int heightinMetres, String position, String foot, String club, int marketValue){
+        setId(id);
+        setFirstname(firstname);
+        setSurname(surname);
+        setAge(age);
+        setDoB(DoB);
+        setNationality(nationality);
+        setHeightinMetres(heightinMetres);
+        setPosition(position);
+        setFoot(foot);
+        setClub(club);
+        setMarketValue(marketValue);
+    }
 }
