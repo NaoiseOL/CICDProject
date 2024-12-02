11bc0d94ab7c208012046cfac0f973f02dcab29d
NaoiseOL <G00385498@atu.ie>
2024-12-02 15:01:41 +0000

    Created a Project and added Player Class

---
+++ src/main/java/com/example/cicdproject/Player.java
@@ -0,0 +1,19 @@
+package com.example.cicdproject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+public class Player {
+
+
+}
